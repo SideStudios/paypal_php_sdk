feb19b5510bf7278a58bb66a71c85b8ab34f9f64
particle6 <kevin@sidestudios.com>
2016-05-09 12:30:12 -0700

    TOOFACED-1235: Added curl setting to explicitly set ssl version

--- lib/shared/PayPalRequest.php
+++ lib/shared/PayPalRequest.php
@@ -95,6 +95,7 @@ abstract class PayPalRequest {
 		curl_setopt($curl_request, CURLOPT_TIMEOUT, 45);
 		curl_setopt($curl_request, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($curl_request, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($curl_request, CURLOPT_SSLVERSION, 6);
 		if ($this->VERIFY_PEER) {
 			curl_setopt($curl_request, CURLOPT_CAINFO, dirname(dirname(__FILE__)) . '/ssl/cert.pem');
 		} else {
